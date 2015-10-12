596d569f96aa467b1c99c81d75c7a897a4131bce
Birunthan Mohanathas <birunthan@mohanathas.com>
2015-10-12 10:55:47 +0300

    Add deploy.sh

---
+++ deploy.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+set -o errexit -o pipefail
+
+script_dir="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
+pushd "$script_dir"
+
+if [ ! -d "_ghpages.git" ]; then
+    git clone --bare -b gh-pages \
+        git@github.com:rainmeter/rainmeter-www.git _ghpages.git
+fi
+
+rm -rf _generated
+hexo generate
+
+export GIT_DIR=_ghpages.git
+export GIT_WORK_TREE=_generated
+
+git config core.autocrlf false
+git fetch --force origin gh-pages
+git reset --mixed FETCH_HEAD
+git checkout -- .nojekyll CNAME
+git add -A .
+git commit -m "Update"
+git push origin gh-pages
+
+popd
