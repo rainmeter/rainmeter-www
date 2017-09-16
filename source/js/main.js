256ff6a87fb4557d1910f01a206013ee686b3d9e
Marco Vockner <marcopixel@live.de>
2017-09-16 17:02:17 +0200

    Tweaked markdown/styling, added items to showcase

--- source/js/main.js
+++ source/js/main.js
@@ -1,4 +1,15 @@
-baguetteBox.run('.gallery', {
-    overlayBackgroundColor: 'rgba(0,0,0,.85)',
-    noScrollbars: true
+$(document).ready(function() {
+  $("[data-fancybox]").fancybox({
+    toolbar: true,
+    buttons: ['fullScreen', 'thumbs', 'close'],
+    smallBtn: false,
+    autoFocus : false,
+    backFocus : false,
+    slideClass : 'showcase',
+    afterShow: function(instance, slide) {
+      // After loading the content, play the content of the slide.
+      var video = slide.$content.get(0);
+      video.play();
+    }
+  });
 });
