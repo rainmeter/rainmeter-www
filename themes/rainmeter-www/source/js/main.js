911f5ce9ba5f90109c312becd6ddc91c26bfd2d0
Marco Vockner <marcopixel@live.de>
2018-07-26 00:39:10 +0200

    initial rework to scss instead of static css, update bootstrap

--- themes/rainmeter-www/source/js/main.js
+++ themes/rainmeter-www/source/js/main.js
@@ -1,11 +1,11 @@
 $(document).ready(function() {
   $("[data-fancybox]").fancybox({
     toolbar: true,
-    buttons: ['fullScreen', 'thumbs', 'close'],
+    buttons: ["fullScreen", "thumbs", "close"],
     smallBtn: false,
-    autoFocus : false,
-    backFocus : false,
-    slideClass : 'showcase',
+    autoFocus: false,
+    backFocus: false,
+    slideClass: "showcase",
     afterShow: function(instance, slide) {
       // After loading the content, play the content of the slide.
       var video = slide.$content.get(0);
@@ -15,9 +15,9 @@ $(document).ready(function() {
 });
 
 $(document).ready(function($) {
-    $(".clickable-row").click(function(e) {
-        if ($(e.target).is("td")) {
-            window.location = $(this).data("href");
-        }
-    });
+  $(".clickable-row").click(function(e) {
+    if ($(e.target).is("td")) {
+      window.location = $(this).data("href");
+    }
+  });
 });
